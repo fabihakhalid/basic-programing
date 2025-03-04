b7bbc185779f5f6fe9d2815aee907946670e7a7b
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-04 13:07:47 -0800

    done by fabiha

---
+++ c.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int myAge; //Variable Declaration
+    printf("Enter Your age") ;
+    scanf("%d", &myAge);
+    printf("Your age is: %d", myAge);
+	return 0;
+}
+
+
