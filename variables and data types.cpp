d72dbce862402533640fe227249aee29f8cff829
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-03 21:00:08 -0800

    done by fabiha

---
+++ variables and data types.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int value1 =9;
+	int value2 =6;
+	  cout<<value1;
+
+   float value =7.9;
+   cout<<value;
+
+   doublevalue1 =172.11;
+   cout<<value1;
+
+   bool condition =true;
+    cout<<true;
+
+	  return 0;
+	  
+}
