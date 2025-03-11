8723672c8c45e9a104e394ed45a410eadd421a80
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-10 21:47:39 -0700

    done

--- operator.cpp
+++ operator.cpp
@@ -8,7 +8,13 @@ int main()
 	// pre increment ,++
 //	printf("%d",++num1);
 	//post incremenet
-	printf("%d",num1++);
+//	printf("%d",num1++);
+
+   int value1,value2;
+   printf("Enter two numbers : ");
+   scanf("%d", &value1);
+   scanf("%d", &value2);
+   printf("%d",value1+value2);
 	
 	
 	
