d313191eb19a1c42fe6f24ee33b8a59b069bf5e1
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-11 13:26:50 -0700

    one

--- task.cpp
+++ task.cpp
@@ -4,10 +4,18 @@ int main()
 {
 	int num1 , num2 ;
 	printf("Enter two integer :");
-	scanf("%d",&num1);
+	scanf("%d/n",&num1);
 	scanf("%d",&num2);
 	int sum = num1+num2;
+	int diff = num1-num2;
+	int product = num1*num2;
+	int divide = num1/num2;
+	int reminder = num1%num2;
 	printf("sum %d", sum );
+	printf("diff %d", diff );
+	printf("product %d", product );
+	printf("divide %d", divide);
+	printf("reminder %d", sum );
 	
 	
 	
