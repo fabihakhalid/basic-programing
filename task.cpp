9f95ed913e385a354c71b9aff66d45eed7384494
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-11 13:18:20 -0700

    done

---
+++ task.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1 , num2 ;
+	printf("Enter two integer :");
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	int sum = num1+num2;
+	printf("sum %d", sum );
+	
+	
+	
+	return 0;
+	
+}
