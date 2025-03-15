eb5b40067e3a5a10f515096638c08437844c27f6
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-14 23:25:17 -0700

    done

---
+++ 3 task.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int num;
+	printf("Enter a number :") ;
+	scanf("%d", &num);
+	
+	printf("post decrement: %d /n",--num);
+	printf("after post decrement : %d/n",num--);
+	
+	printf("pre decrement : %d /n",--num);
+	
+	
+	return 0;
+}
