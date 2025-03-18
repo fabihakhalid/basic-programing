63128c60fe9428934efc7acdad44121f0f885401
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-18 12:15:14 -0700

    done by fabiha

---
+++ controll statement.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+
+int day;
+    printf("enter a day: \n");
+    scanf("%d", &day);
+    
+    switch(day){
+    	case 1:
+    		printf("monday");
+    		break;
+    		case 2:
+    		printf("tuesday");
+    		break;
+    		case 3:
+    		printf("wednesday");
+    		break;
+    		case 4:
+    		printf("thursday");
+    		break;
+    		case 5:
+    		printf("friday");
+    		break;
+    		case 6:
+    		printf("sturday");
+    		break;
+    		case 7:
+    		printf("sunday");
+    		break;
+    		defult:
+    			printf("invaled day");
+}
+    return 0;
+}
+
---
+++ marks program.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+	
+	int marks;
+	printf("enter your marks: \n");
+	scanf("%d" , &marks);
+	
+	switch(marks){
+		case 1:
+			printf("100");
+			break;
+			case 2:
+			printf("90");
+			break;
+			case 3:
+			printf("80");
+			break;
+			case 4:
+			printf("70");
+			break;
+			case 5:
+			printf("60");
+			break;
+			defult:
+				printf("you are fail");
+	}
+	
+	return 0;
+	
+}
