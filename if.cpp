3a764313e5ba625cc6c1e44812f683d07d30874b
fabihakhalid <fabihakhalid2005@gmail.com>
2025-03-18 21:45:40 -0700

    done

---
+++ if.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main ()
+{
+	
+	 printf "Enter your marks :"
+	if(the number is <30);{
+	printf "fail :"
+}
+ else {
+ 	(the number is >70);
+ 	printf "pass :";
+ }
+	
+	
+	
+	return 0;
+	
+}
