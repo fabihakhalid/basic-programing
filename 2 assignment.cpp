412cbe96273b0b5b86dbcc219ec55bfc1f7e83da
Fabiha Khalid <fabihakhalid2005@gmail.com>
2025-05-02 10:01:33 -0700

    Update 2 assignment.cpp

--- 2 assignment.cpp
+++ 2 assignment.cpp
@@ -8,7 +8,7 @@ int main() {
     // Outer loop (first number)
     for (i = 1; i <= 2; i++) {
         // Inner loop (second number)
-        for (j = 1; j <= 5; j++) {
+        for (j = 1; j <= 5; j++) { 
             printf("%d-%d\n", i, j);
         }
     }
