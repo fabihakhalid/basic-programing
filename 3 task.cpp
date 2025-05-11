abd71e77eec452fec0db309135358424a544cf62
Fabiha Khalid <fabihakhalid2005@gmail.com>
2025-05-11 09:26:03 -0700

    Update 3 task.cpp

--- 3 task.cpp
+++ 3 task.cpp
@@ -3,14 +3,13 @@
 int main()
 {
 	int num;
-	printf("Enter a number :") ;
+	printf("Enter a number :") ; 
 	scanf("%d", &num);
 	
 	printf("post decrement: %d /n",--num);
 	printf("after post decrement : %d/n",num--);
 	
 	printf("pre decrement : %d /n",--num); 
-	
 	
 	return 0;
 }
