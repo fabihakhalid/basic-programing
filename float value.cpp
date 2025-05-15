1bc9fce117b4e4c4f60059f9a9ed450d36922e4e
Fabiha Khalid <fabihakhalid2005@gmail.com>
2025-05-15 08:55:09 -0700

    Update float value.cpp

--- float value.cpp
+++ float value.cpp
@@ -8,7 +8,7 @@ int main() {
 
     avg = (value1 + value2 + value3 + value4) / 4;
 
-    printf("The Average of %.2f, %.2f, %.2f and %.2f is: %.2f\n", value1, value2, value3, value4, avg);
+    printf("The Average of %.2f, %.2f, %.2f and %.2f is: %.2f\n", value1, value2, value3, value4, avg); 
 
     return 0;
 }
